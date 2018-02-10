3666c521146cfac15ed5c5324a77f9e895a25c96
yongrenwang <czie.alexmak@gmail.com>
2018-02-10 12:26:26 +0800

    Initial Commit

---
+++ Seoul Tourism Uploader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seoul Tourism Uploader
+//
+//  Created by Aleksander Makedonski on 2/10/18.
+//  Copyright © 2018 Aleksander Makedonski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
